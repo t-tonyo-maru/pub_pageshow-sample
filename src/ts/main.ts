15de1ec8d8568d2616009705cd5d52b6c4414c8e
t-tonyo-maru <atsuji@atsujinoMacBook-Pro.local>
2022-09-04 02:09:03 +0900

    add: $.cookieの処理を実装

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,12 @@
 import $ from 'jquery'
+import 'jquery.cookie'
 
 $(() => {
-  console.log(this)
+  // cookie hoge をセット
+  $.cookie('hoge', 'hoge content', {
+    domain: window.location.hostname,
+    path: '/'
+  })
+  // cookie を表示
+  $('.js-show-cookie').text(`${$.cookie('hoge')}`)
 })
--- src/ts/sub.ts
+++ src/ts/sub.ts
@@ -1,5 +1,16 @@
 import $ from 'jquery'
+import 'jquery.cookie'
 
 $(() => {
-  console.log(this)
+  // cookie hoge をセット
+  $.cookie('hoge', 'updated hoge content', {
+    domain: window.location.hostname,
+    path: '/'
+  })
+  // cookie を表示
+  $('.js-show-cookie').text(`${$.cookie('hoge')}`)
+
+  setTimeout(() => {
+    window.history.back()
+  }, 10 * 1000)
 })
