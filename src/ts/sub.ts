ebe326e26ecb51acc399ba51c875ff9373b5c3bf
t-tonyo-maru <atsuji@atsujinoMacBook-Pro.local>
2022-09-04 01:48:59 +0900

    fix: 各種ファイルを整理

--- src/ts/sub.ts
+++ src/ts/sub.ts
@@ -1,10 +1,5 @@
-// import { createPadZero } from './module/zeroPadding/zeroPadding'
+import $ from 'jquery'
 
-window.onload = () => {
-  console.log('hello!! world!!')
-
-  // 開発と本番のどちらの環境かを取得
-  console.log(process.env.NODE_ENV) // development || production
-  // 環境変数を取得
-  console.log(process.env.APP_URL)
-}
+$(() => {
+  console.log(this)
+})
